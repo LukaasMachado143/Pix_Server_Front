00de44c75f0481f0f6526d11b99ea411fac935d2
Lucas Fernando <lucas.fernando@predify.me>
2023-11-25 23:29:16 -0300

    feat: created login form and centralizong content.

--- src/views/auth/auth-main.vue
+++ src/views/auth/auth-main.vue
@@ -1,13 +1,24 @@
 <template>
-  <h1>Página de Autenticação</h1>
+  <div class="main-container">
+    <h1>Pix Server</h1>
+    <login-form />
+  </div>
 </template>
 
 <script>
+import LoginForm from './components/login-form.vue'
 export default {
+  components: { LoginForm }
 
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.main-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100%;
+}
 </style>
---
+++ src/views/auth/components/login-form.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Formulário de login</h1>
+</template>
+
+<script>
+export default {
+    name: "LoginForm"
+
+}
+</script>
+
+<style></style>
