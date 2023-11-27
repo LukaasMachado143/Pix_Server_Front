5f385c1b23bdf924d40088d3043c41b29a5f324f
Lucas Fernando <lucas.fernando@predify.me>
2023-11-27 09:49:34 -0300

    feat: update language.

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,12 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
+import pt from "vuetify/src/locale/pt.ts";
 export default new Vuetify({
+  lang: {
+    locales: { pt },
+    current: "pt",
+  },
 });
