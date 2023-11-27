7fa4123b8a73faf448eaad3c360f741032f218e5
Lucas Fernando <lucas.fernando@predify.me>
2023-11-27 16:22:28 -0300

    featÇ creating component slider transfers.

---
+++ src/views/main/list-transfer/components/card-carousel-transfer.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card class="pa-4">
+    <h3 class="mb-2">{{ data.title }}:</h3>
+    <v-slide-group multiple show-arrows>
+      <v-slide-item>
+        <v-card color="red" width="250px" height="250px" class="mr-2 pa-1">
+          <v-row no-gutters align="center">
+            <span>Data:</span>
+            <span>Data:</span>
+          </v-row>
+        </v-card>
+      </v-slide-item>
+    </v-slide-group>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ['data']
+
+}
+</script>
+
+<style></style>
--- src/views/main/list-transfer/list-transfer-main.vue
+++ src/views/main/list-transfer/list-transfer-main.vue
@@ -1,10 +1,29 @@
 <template>
-    <h1 class="white--text">Testando</h1>
+	<div>
+		<card-carousel-transfer v-for="transfers, index in transfersList" :key="index"
+			:class="index == transfersList.length - 1 ? '' : 'mb-4'" :data="transfers" />
+	</div>
 </template>
 
 <script>
+import CardCarouselTransfer from './components/card-carousel-transfer.vue'
 export default {
+	components: { CardCarouselTransfer },
+	data() {
+		return {
+			transfersList: [
+				{
+					title: 'Recebidas',
+					type: "recived"
+				},
+				{
+					title: 'Enviadas',
+					type: "sended"
+				},
 
+			]
+		}
+	}
 }
 </script>
 
