ec72b27973c550bc8ac81eb1507e6425b288a48c
Lucas Fernando <lucas.fernando@predify.me>
2023-11-26 21:21:08 -0300

    feat: set background imgae.

--- src/views/auth/auth-main.vue
+++ src/views/auth/auth-main.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="main-container">
-    <h1>Pix Server</h1>
-    <login-form />
+    <h1 class="white--text">Pix Server</h1>
+    <v-card elevation="24" class="pa-4 mt-10" width="350">
+      <login-form />
+    </v-card>
   </div>
 </template>
 
@@ -15,6 +17,10 @@ export default {
 
 <style lang="scss" scoped>
 .main-container {
+  background-image: url("https://png.pngtree.com/thumb_back/fw800/background/20190718/pngtree-modern-dark-background-with-shine-hexagon-steel-design-image_283323.jpg");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
   display: flex;
   flex-direction: column;
   justify-content: center;
