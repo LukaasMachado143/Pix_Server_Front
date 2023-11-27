f08412fbfa19bc09fc7bf4c1c804639d04d482db
Lucas Fernando <lucas.fernando@predify.me>
2023-11-26 21:53:24 -0300

    feat: created register form.

--- src/views/auth/auth-main.vue
+++ src/views/auth/auth-main.vue
@@ -1,16 +1,30 @@
 <template>
   <div class="main-container">
     <h1 class="white--text">Pix Server</h1>
-    <v-card elevation="24" class="pa-4 mt-10" width="350">
-      <login-form />
+    <v-card elevation="24" class="pa-4 mt-10" width="350" color="grey lighten-4">
+      <div v-if="isLogin">
+        <login-form />
+        <p class="text-center mt-2" style="cursor: pointer;" @click="isLogin = !isLogin">Clique aqui para se cadastrar !</p>
+      </div>
+      <div v-else>
+        <register-form />
+        <p class="text-center mt-2" style="cursor: pointer;" @click="isLogin = !isLogin">Clique aqui para se entrar !</p>
+
+      </div>
     </v-card>
   </div>
 </template>
 
 <script>
 import LoginForm from './components/login-form.vue'
+import RegisterForm from './components/register-form.vue'
 export default {
-  components: { LoginForm }
+  components: { LoginForm, RegisterForm },
+  data() {
+    return {
+      isLogin: true,
+    }
+  }
 
 }
 </script>
