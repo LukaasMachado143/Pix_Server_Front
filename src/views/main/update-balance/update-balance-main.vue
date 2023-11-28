4b01dee1c9f739c540d8a539aee22d2fc3a5c708
Lucas Fernando <lucas.fernando@predify.me>
2023-11-28 18:53:37 -0300

    feat: import new components.

--- src/views/main/update-balance/update-balance-main.vue
+++ src/views/main/update-balance/update-balance-main.vue
@@ -1,10 +1,22 @@
 <template>
-    <h1>Página de alteração de Saldo</h1>
+  <v-row no-gutters justify="center">
+    <v-col cols="12" sm="auto" class="pr-2">
+      <add-card />
+    </v-col>
+    <v-col cols="12" sm="auto" class="pl-2">
+      <spline-chart />
+    </v-col>
+
+  </v-row>
 </template>
 
 <script>
+import SplineChart from './components/spline-chart.vue'
+import addCard from './components/add-card.vue'
 export default {
-    name: 'UpdateBalanceMain'
+  components: { addCard, SplineChart },
+  name: 'UpdateBalanceMain',
+
 }
 </script>
 
