5352dc3b6da93507c7a954d3d49a4007611c8bb5
Lucas Fernando <lucas.fernando@predify.me>
2023-11-28 18:41:26 -0300

    fix: fixing real name of route.

--- src/views/main/list-transfer/components/redirect-add-balance.vue
+++ src/views/main/list-transfer/components/redirect-add-balance.vue
@@ -1,6 +1,6 @@
 <template>
   <v-row no-gutters align="center">
-    <v-icon color="green" size="50" class="mr-2" @click="$router.push({ name: 'teste' })">
+    <v-icon color="green" size="50" class="mr-2" @click="$router.push({ name: 'updateBalance' })">
       mdi-plus-box
     </v-icon>
     <h3 class="white--text">Adicionar Saldo ...</h3>
