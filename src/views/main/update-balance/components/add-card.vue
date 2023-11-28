2237bfafc4744e3c440ccf610368762f2e53763e
Lucas Fernando <lucas.fernando@predify.me>
2023-11-28 18:52:53 -0300

    feat: created card for add balance.

---
+++ src/views/main/update-balance/components/add-card.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card class="pa-4" width="300px">
+    <h2 class="text-center mb-2">Adicionar Saldo</h2>
+    <v-text-field outlined dense hide-details="auto" prefix="R$" :suffix="balance ? '' : ',00'" v-model="balance"
+      type="number" min="0" class="mb-4" />
+    <v-btn color="light-blue lighten-1" width="100%">
+      <span class="white--text">Salvar</span>
+    </v-btn>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      balance: null,
+    }
+  }
+
+}
+</script>
+
+<style></style>
