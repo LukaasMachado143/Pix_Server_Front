4fbae298a6bd7ea2a02b43f587ff20176de4ad39
Lucas Fernando <lucas.fernando@predify.me>
2023-11-28 18:23:25 -0300

    feat: created and import new component, the column chart.

---
+++ src/views/main/list-transfer/components/charts/column-chart.vue
@@ -0,0 +1,89 @@
+<template>
+	<highcharts class="hc" :options="chartOptions" />
+</template>
+
+<script>
+import Highcharts from "highcharts";
+
+// Exporta dados em PNG, JPEG, PDF, SVG, imprime e visualiza tela cheia
+import exportingInit from "highcharts/modules/exporting";
+exportingInit(Highcharts);
+
+// Exporta dados CSV, XLS, e muda visualização por tabela
+import exportData from "highcharts/modules/export-data";
+exportData(Highcharts);
+
+import HighchartsMore from "highcharts/highcharts-more";
+HighchartsMore(Highcharts);
+
+// When chart have no data
+import noData from "highcharts/modules/no-data-to-display";
+noData(Highcharts);
+import setCurrency from "@/utils/setCurrency";
+export default {
+	data() {
+		return {
+			chartOptions: {
+				chart: {
+					type: 'column',
+				},
+				title: {
+					text: 'Valor Acumulado'
+				},
+				xAxis: {
+					visible: false
+				},
+				yAxis: {
+					visible: false,
+					title: {
+						text: ''
+					},
+					labels: {
+						formatter: function () {
+							return setCurrency(this.value);
+						}
+					}
+				},
+				tooltip: {
+					enabled: false
+				},
+				series: [{
+					name: 'Recebidas',
+					data: [5.2],
+					color: 'green',
+					dataLabels: {
+						enabled: true,
+						formatter: function () {
+							return setCurrency(this.y);
+						},
+						align: 'center',
+						verticalAlign: 'top',
+						y: -20
+					}
+
+				}, {
+					name: 'Enviadas',
+					data: [5.2],
+					color: 'red',
+					dataLabels: {
+						enabled: true,
+						formatter: function () {
+							return setCurrency(this.y);
+						},
+						align: 'center',
+						verticalAlign: 'top',
+						y: -20
+					}
+
+				}]
+			}
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+.hc {
+	border-radius: 10px;
+}
+</style>
--- src/views/main/list-transfer/list-transfer-main.vue
+++ src/views/main/list-transfer/list-transfer-main.vue
@@ -3,31 +3,24 @@
     <redirect-add-balance />
     <card-carousel-transfer v-for="transfers, index in transfersList" :key="index"
       :class="index == transfersList.length - 1 ? '' : 'mb-4'" :data="transfers" />
-    <highcharts class="hc" :options="chartOptions" />
+    <v-row no-gutters justify="center" class="mt-4 px-4">
+      <v-col cols="12" sm="6" class="pr-2">
+        <column-chart />
+      </v-col>
+      <v-col cols="12" sm="6" class="pl-2">
+        <column-chart />
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
 import CardCarouselTransfer from './components/card-carousel-transfer.vue'
+import ColumnChart from './components/charts/column-chart.vue'
 import RedirectAddBalance from './components/redirect-add-balance.vue'
-import Highcharts from "highcharts";
 
-// Exporta dados em PNG, JPEG, PDF, SVG, imprime e visualiza tela cheia
-import exportingInit from "highcharts/modules/exporting";
-exportingInit(Highcharts);
-
-// Exporta dados CSV, XLS, e muda visualização por tabela
-import exportData from "highcharts/modules/export-data";
-exportData(Highcharts);
-
-import HighchartsMore from "highcharts/highcharts-more";
-HighchartsMore(Highcharts);
-
-// When chart have no data
-import noData from "highcharts/modules/no-data-to-display";
-noData(Highcharts);
 export default {
-  components: { CardCarouselTransfer, RedirectAddBalance },
+  components: { CardCarouselTransfer, RedirectAddBalance, ColumnChart },
   data() {
     return {
       transfersList: [
@@ -61,77 +54,7 @@ export default {
         },
 
       ],
-      chartOptions: {
-        chart: {
-          type: 'spline'
-        },
-        title: {
-          text: 'Monthly Average Temperature'
-        },
-        subtitle: {
-          text: 'Source: ' +
-            '<a href="https://en.wikipedia.org/wiki/List_of_cities_by_average_temperature" ' +
-            'target="_blank">Wikipedia.com</a>'
-        },
-        xAxis: {
-          categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-            'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-          accessibility: {
-            description: 'Months of the year'
-          }
-        },
-        yAxis: {
-          title: {
-            text: 'Temperature'
-          },
-          labels: {
-            format: '{value}°'
-          }
-        },
-        tooltip: {
-          crosshairs: true,
-          shared: true
-        },
-        plotOptions: {
-          spline: {
-            marker: {
-              radius: 4,
-              lineColor: '#666666',
-              lineWidth: 1
-            }
-          }
-        },
-        series: [{
-          name: 'Tokyo',
-          marker: {
-            symbol: 'square'
-          },
-          data: [5.2, 5.7, 8.7, 13.9, 18.2, 21.4, 25.0, {
-            y: 26.4,
-            marker: {
-              symbol: 'url(https://www.highcharts.com/samples/graphics/sun.png)'
-            },
-            accessibility: {
-              description: 'Sunny symbol, this is the warmest point in the chart.'
-            }
-          }, 22.8, 17.5, 12.1, 7.6]
 
-        }, {
-          name: 'Bergen',
-          marker: {
-            symbol: 'diamond'
-          },
-          data: [{
-            y: 1.5,
-            marker: {
-              symbol: 'url(https://www.highcharts.com/samples/graphics/snow.png)'
-            },
-            accessibility: {
-              description: 'Snowy symbol, this is the coldest point in the chart.'
-            }
-          }, 1.6, 3.3, 5.9, 10.5, 13.5, 14.5, 14.4, 11.5, 8.7, 4.7, 2.6]
-        }]
-      }
     }
   }
 }
