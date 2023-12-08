2579c4f3402ecb107ae684b9ad259f55c82cbe14
Lucas Fernando <lucas.fernando@predify.me>
2023-12-08 12:33:39 -0300

    feat: created new card for change password of user.

---
+++ src/views/main/edit-user-data/components/password-change.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-card class="pa-4" height="100%">
+    <v-text-field color="blue" label="Senha Antiga" append-icon="mdi-lock" />
+    <v-text-field color="blue" label="Nova Senha" append-icon="mdi-lock" />
+    <v-btn color="light-blue lighten-1" width="100%">
+      <span class="white--text">Salvar</span>
+    </v-btn>
+  </v-card>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
