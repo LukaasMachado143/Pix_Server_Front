a88be3ba67a16c7812687964e27005d5e65d6ee2
Lucas Fernando <lucas.fernando@predify.me>
2023-12-08 14:24:20 -0300

    feat: created axios config for integration with back.

---
+++ src/configs/axios.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+function getURL() {
+    if (
+        window.location.host.includes("192.168.18.22:8080") ||
+        window.location.host.includes("localhost:8080") ||
+        window.location.host.includes("192.168.18.21:8080")
+    ) {
+        return "http://localhost:3000";
+    } else {
+        return "https://bricka-dida.vercel.app";
+    }
+}
+
+const baseURL = getURL();
+
+var api = axios.create({
+    baseURL,
+});
+
+// api.interceptors.request.use(function (config) {
+//     const auth = JSON.parse(localStorage.getItem("userDataManagementBuffet"));
+//     if (auth) config.headers["Authorization"] = "Bearer " + auth.token;
+//     return config;
+// });
+
+export default api;
