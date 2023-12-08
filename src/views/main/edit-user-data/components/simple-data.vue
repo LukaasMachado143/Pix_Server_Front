ed1d7c70f53d8645c2f539743638c9c7dad3ce0d
Lucas Fernando <lucas.fernando@predify.me>
2023-12-08 12:33:21 -0300

    feat: created new card for change simple information about user.

---
+++ src/views/main/edit-user-data/components/simple-data.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card class="pa-4" height="100%">
+    <v-text-field color="blue" label="Email" append-icon="mdi-email" />
+    <v-text-field color="blue" label="Usuário" append-icon="mdi-account" />
+    <v-text-field color="blue" label="Chave Pix" append-icon="mdi-key" />
+    <v-btn color="light-blue lighten-1" width="100%">
+      <span class="white--text">Salvar</span>
+    </v-btn>
+  </v-card>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
