76e2148410030148e8b1534ba3bbb29108c8980e
Lucas Fernando <lucas.fernando@predify.me>
2023-12-09 20:22:06 -0300

    feat: added validation for update balance and identation for code.

--- src/views/main/list-transfer/list-transfer-main.vue
+++ src/views/main/list-transfer/list-transfer-main.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
-    <redirect-add-balance />
-    <card-carousel-transfer v-for="transfers, index in transfersList" :key="index"
-      :class="index == transfersList.length - 1 ? '' : 'mb-4'" :data="transfers" />
+    <redirect-add-balance v-if="balanceAccount && balanceAccount <= 100" />
+    <card-carousel-transfer
+      v-for="(transfers, index) in transfersList"
+      :key="index"
+      :class="index == transfersList.length - 1 ? '' : 'mb-4'"
+      :data="transfers"
+    />
     <v-row no-gutters justify="center" class="mt-4 px-4">
       <v-col cols="12" sm="6" class="pr-2">
         <column-chart />
@@ -15,49 +19,56 @@
 </template>
 
 <script>
-import CardCarouselTransfer from './components/card-carousel-transfer.vue'
-import ColumnChart from './components/charts/column-chart.vue'
-import SplineChart from './components/charts/spline-chart.vue'
-import RedirectAddBalance from './components/redirect-add-balance.vue'
+import CardCarouselTransfer from "./components/card-carousel-transfer.vue";
+import ColumnChart from "./components/charts/column-chart.vue";
+import SplineChart from "./components/charts/spline-chart.vue";
+import RedirectAddBalance from "./components/redirect-add-balance.vue";
 
 export default {
-  components: { CardCarouselTransfer, RedirectAddBalance, ColumnChart, SplineChart },
+  components: {
+    CardCarouselTransfer,
+    RedirectAddBalance,
+    ColumnChart,
+    SplineChart,
+  },
   data() {
     return {
       transfersList: [
         {
-          title: 'Recebidas',
+          title: "Recebidas",
           type: "recived",
           list: [
-            { date: '2023-11-27T00:01:00', value: 150 },
-            { date: '2023-11-27T00:01:00', value: 200 },
-            { date: '2023-11-27T00:01:00', value: 10 },
-            { date: '2023-11-27T00:01:00', value: 63 },
-            { date: '2023-11-27T00:01:00', value: 150 },
-            { date: '2023-11-27T00:01:00', value: 856 },
-            { date: '2023-11-27T00:01:00', value: 45 },
-            { date: '2023-11-27T00:01:00', value: 4 },
-          ]
+            { date: "2023-11-27T00:01:00", value: 150 },
+            { date: "2023-11-27T00:01:00", value: 200 },
+            { date: "2023-11-27T00:01:00", value: 10 },
+            { date: "2023-11-27T00:01:00", value: 63 },
+            { date: "2023-11-27T00:01:00", value: 150 },
+            { date: "2023-11-27T00:01:00", value: 856 },
+            { date: "2023-11-27T00:01:00", value: 45 },
+            { date: "2023-11-27T00:01:00", value: 4 },
+          ],
         },
         {
-          title: 'Enviadas',
+          title: "Enviadas",
           type: "sended",
           list: [
-            { date: '2023-11-27T00:01:00', value: 12 },
-            { date: '2023-11-27T00:01:00', value: 1 },
-            { date: '2023-11-27T00:01:00', value: 150 },
-            { date: '2023-11-27T00:01:00', value: 150 },
-            { date: '2023-11-27T00:01:00', value: 152310 },
-            { date: '2023-11-27T00:01:00', value: 2 },
-            { date: '2023-11-27T00:01:00', value: 2232 },
-            { date: '2023-11-27T00:01:00', value: 224 },
-          ]
+            { date: "2023-11-27T00:01:00", value: 12 },
+            { date: "2023-11-27T00:01:00", value: 1 },
+            { date: "2023-11-27T00:01:00", value: 150 },
+            { date: "2023-11-27T00:01:00", value: 150 },
+            { date: "2023-11-27T00:01:00", value: 152310 },
+            { date: "2023-11-27T00:01:00", value: 2 },
+            { date: "2023-11-27T00:01:00", value: 2232 },
+            { date: "2023-11-27T00:01:00", value: 224 },
+          ],
         },
-
       ],
-    }
-  }
-}
+    };
+  },
+  computed: {
+    balanceAccount() {
+      return this.$store.getters["userStore/userData"].balance;
+    },
+  },
+};
 </script>
-
-<style></style>
