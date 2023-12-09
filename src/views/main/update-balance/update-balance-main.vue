05a891b61d20b48236bf15d9814f9ca0575997a6
Lucas Fernando <lucas.fernando@predify.me>
2023-12-09 20:19:06 -0300

    feat: add validation for update balance.

--- src/views/main/update-balance/update-balance-main.vue
+++ src/views/main/update-balance/update-balance-main.vue
@@ -6,18 +6,38 @@
     <v-col cols="12" sm="auto" class="pl-2">
       <spline-chart />
     </v-col>
-
   </v-row>
 </template>
 
 <script>
-import SplineChart from './components/spline-chart.vue'
-import addCard from './components/add-card.vue'
+import SplineChart from "./components/spline-chart.vue";
+import addCard from "./components/add-card.vue";
+import setCurrency from "@/utils/setCurrency";
 export default {
   components: { addCard, SplineChart },
-  name: 'UpdateBalanceMain',
-
-}
+  name: "UpdateBalanceMain",
+  computed: {
+    balanceAccount() {
+      return this.$store.getters["userStore/userData"].balance;
+    },
+  },
+  methods: {
+    setlistTransferPage() {
+      if (this.balanceAccount && this.balanceAccount >= 100) {
+        this.$router.push({ name: "listTransfer" });
+        this.$store.commit("snackbarStore/set", {
+          message: `Só é possível adicionar salde se o mesmo estiver abaixo de ${setCurrency(
+            100
+          )}.`,
+          type: "info",
+        });
+      }
+    },
+  },
+  mounted() {
+    this.setlistTransferPage();
+  },
+};
 </script>
 
 <style></style>
