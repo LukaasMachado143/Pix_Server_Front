fee49e8ee50d78fe73ca8465737f953875a729f8
Lucas Fernando <lucas.fernando@predify.me>
2023-12-09 20:17:18 -0300

    feat: remove validation for new styling

--- src/utils/setCurrency.js
+++ src/utils/setCurrency.js
@@ -1,5 +1,5 @@
 export default function setCurrency(value) {
-  if (!value) return "-";
+  // if (!value) return "R$ 00,00";
   const positiveValue = Math.abs(value);
   return positiveValue.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
 }
