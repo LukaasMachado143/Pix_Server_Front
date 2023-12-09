c7512be9b5df6a862e99b854ef67e54cb41f3a96
Lucas Fernando <lucas.fernando@predify.me>
2023-12-09 20:19:26 -0300

    feat: added validation for current page.

--- src/components/app-bar/components/router-list.vue
+++ src/components/app-bar/components/router-list.vue
@@ -1,7 +1,7 @@
 <template>
   <v-list>
-    <v-list-item v-for="page, index in list" :key="index">
-      <v-list-item-title @click="$router.push({ name: page.to })" style="cursor: pointer;">
+    <v-list-item v-for="(page, index) in list" :key="index">
+      <v-list-item-title @click="go(page.to)" style="cursor: pointer">
         <v-row no-gutters align="end">
           <v-icon class="mr-2">
             {{ page.icon }}
@@ -15,8 +15,18 @@
 
 <script>
 export default {
-  props: ['list']
-}
+  props: ["list"],
+  methods: {
+    go(to) {
+      if (this.$route.name != to) this.$router.push({ name: to });
+      else
+        this.$store.commit("snackbarStore/set", {
+          message: "Você está nessa página !",
+          type: "info",
+        });
+    },
+  },
+};
 </script>
 
 <style></style>
