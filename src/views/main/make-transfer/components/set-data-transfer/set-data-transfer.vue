2b37d2673c8f6aa1dd04888053466beb9996055e
Lucas Fernando <lucas.fernando@predify.me>
2023-12-10 20:19:58 -0300

    feat: setup for conected user and update username for name.

--- src/views/main/make-transfer/components/set-data-transfer/set-data-transfer.vue
+++ src/views/main/make-transfer/components/set-data-transfer/set-data-transfer.vue
@@ -1,57 +1,73 @@
 <template>
-	<v-card class="pa-4" height="100%" :disabled="!selectedUser">
-		<h3 class="mb-4">Dados da Transferência</h3>
-		<h3>De:</h3>
-		<user-data :data="conectedUser" />
-		<h3 class="mt-2">Para:</h3>
-		<user-data :data="selectedUser" class="mb-4" />
-		<v-row no-gutters align="center">
-			<span class="font-weight-bold mr-2">Valor da Transferência:</span>
-			<v-text-field outlined dense hide-details="auto" prefix="R$" :suffix="trasnferValue ? '' : ',00'"
-				v-model="trasnferValue" type="number" min="0" />
-		</v-row>
-		<v-row no-gutters justify="end" class="mt-4">
-			<v-btn color="blue" class="mr-4" outlined @click="$emit('resetSelectedUser')">
-				<span>cancelar</span>
-			</v-btn>
-			<v-btn color="green">
-				<span class="white--text">transferir</span>
-			</v-btn>
-		</v-row>
-	</v-card>
+  <v-card class="pa-4" height="100%" :disabled="!selectedUser">
+    <h3 class="mb-4">Dados da Transferência</h3>
+    <h3>De:</h3>
+    <user-data :data="conectedUser" />
+    <h3 class="mt-2">Para:</h3>
+    <user-data :data="selectedUser" class="mb-4" />
+    <v-row no-gutters align="center">
+      <span class="font-weight-bold mr-2">Valor da Transferência:</span>
+      <v-text-field
+        outlined
+        dense
+        hide-details="auto"
+        prefix="R$"
+        :suffix="trasnferValue ? '' : ',00'"
+        v-model="trasnferValue"
+        type="number"
+        min="0"
+      />
+    </v-row>
+    <v-row no-gutters justify="end" class="mt-4">
+      <v-btn
+        color="blue"
+        class="mr-4"
+        outlined
+        @click="$emit('resetSelectedUser')"
+      >
+        <span>cancelar</span>
+      </v-btn>
+      <v-btn color="green" :disabled="!trasnferValue">
+        <span class="white--text">transferir</span>
+      </v-btn>
+    </v-row>
+  </v-card>
 </template>
 
 <script>
-import userData from './user-data.vue'
+import userData from "./user-data.vue";
 export default {
-	components: { userData },
-	props: ['selectedUser'],
-	data() {
-		return {
-			conectedUser: null,
-			trasnferValue: null,
-		}
-	}
-	,
-	watch: {
-		selectedUser: {
-			handler(value) {
-				if (value) this.conectedUser = {
-					imagePath: "https://randomuser.me/api/portraits/women/85.jpg",
-					username: 'Nome do usuário conectado',
-					phone: '(xx) xxxxx-xxxx',
-					pixKey: '1'
-				}
-				else this.conectedUser = null
-			}
-		},
-		trasnferValue: {
-			handler(value) {
-				if (value < 0) this.trasnferValue = null
-			}
-		}
-
-	}
-
-}
+  components: { userData },
+  props: ["selectedUser"],
+  data() {
+    return {
+      conectedUser: null,
+      trasnferValue: null,
+    };
+  },
+  methods: {
+    setConectedUser() {
+      const userData = this.$store.getters["userStore/userData"];
+      return {
+        imagePath: "https://randomuser.me/api/portraits/women/85.jpg",
+        name: userData.name,
+        phone: userData.phone,
+        pixKey: userData.pixKey,
+      };
+    },
+  },
+  watch: {
+    selectedUser: {
+      handler(value) {
+        if (value) this.conectedUser = this.setConectedUser();
+        else this.conectedUser = null;
+      },
+    },
+    trasnferValue: {
+      handler(value) {
+        if (value < 0) this.trasnferValue = null;
+      },
+    },
+  },
+};
 </script>
