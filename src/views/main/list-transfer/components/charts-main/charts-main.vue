7d572243884dc4ef6fe31ecd79cfc4ec0ab524dc
Lucas Fernando <lucas.fernando@predify.me>
2023-12-11 13:24:55 -0300

    feat: refactored components.

---
+++ src/views/main/list-transfer/components/charts-main/charts-main.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-row no-gutters justify="center" class="mt-4 px-4">
+    <v-col cols="12" sm="6" class="pr-2">
+      <Column-chart />
+    </v-col>
+    <v-col cols="12" sm="6" class="pl-2">
+      <Spline-chart />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import ColumnChart from "./components/column-chart.vue";
+import SplineChart from "./components/spline-chart.vue";
+export default {
+  components: { ColumnChart, SplineChart },
+};
+</script>
+
+<style>
+</style>
--- src/views/main/list-transfer/list-transfer-main.vue
+++ src/views/main/list-transfer/list-transfer-main.vue
@@ -1,30 +1,21 @@
 <template>
   <div>
-    <redirect-add-balance v-if="balanceAccount && balanceAccount <= 100" />
+    <Redirect-add-balance v-if="balanceAccount && balanceAccount <= 100" />
     <Carousel-main />
-    <v-row no-gutters justify="center" class="mt-4 px-4">
-      <v-col cols="12" sm="6" class="pr-2">
-        <column-chart />
-      </v-col>
-      <v-col cols="12" sm="6" class="pl-2">
-        <spline-chart />
-      </v-col>
-    </v-row>
+    <Charts-main />
   </div>
 </template>
 
 <script>
 import CarouselMain from "./components/carousel-main/carousel-main.vue";
-import ColumnChart from "./components/charts/column-chart.vue";
-import SplineChart from "./components/charts/spline-chart.vue";
+import ChartsMain from "./components/charts-main/charts-main.vue";
 import RedirectAddBalance from "./components/redirect-add-balance.vue";
 
 export default {
   components: {
     RedirectAddBalance,
-    ColumnChart,
-    SplineChart,
     CarouselMain,
+    ChartsMain,
   },
   data() {
     return {};
